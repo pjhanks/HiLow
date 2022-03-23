b0a05378d0e5601898a8205de0b41eae93f6dc45
pjhanks <pjhanks@uwm.edu>
2022-03-23 13:54:29 -0500

    Test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
 RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack',
               'Queen', 'King')
 
-NCARDS = 8
+NCARDS = 4
 
 # Pass in a deck and this function returns a random card from the deck
 def getCard(deckListIn):
